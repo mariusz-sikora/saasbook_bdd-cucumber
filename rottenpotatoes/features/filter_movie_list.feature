3705fc0d59fcad7d1db0ad10ac2c91d9a9cbf444
OpenShift guest <msikora87@gmail.com>
2014-06-03 15:47:21 -0400

    home work

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -23,10 +23,22 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: PG, R
+  
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: G, PG-13
+  
   # enter step to "submit" the search form on the homepage
+  When I press "Refresh"
+  
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see movies with rating PG, R
+  
   # enter step(s) to ensure that other movies are not visible
+  And I should not see movies with rating G, PG-13
 
 Scenario: all ratings selected
   # see assignment
+  Given I check all checkbox
+  When I press "Refresh"
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,6 +1,8 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  # I've got busy sqlite exception -> fixed by new test2 db
+  Movie.delete_all
   movies_table.hashes.each do |movie|
       Movie.create!(:title => movie[:title], :rating => movie[:rating],
                     :release_date => movie[:release_date])
@@ -9,13 +11,29 @@ Given /the following movies exist/ do |movies_table|
   end
 end
 
+Given(/^I check all checkbox$/) do
+  Movie.all_ratings.each do |rating|
+      step("I check \"ratings_#{rating}\"")
+  end
+end
+
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+    @movies_at_side = []
+    all('#movies tr > td:nth-child(1)').each do |movie|
+       @movies_at_side << movie.text.to_s 
+    end
+    
+    f1 = @movies_at_side.index(e1)
+    f2 = @movies_at_side.index(e2)
+    
+    if f1.kind_of?(Numeric) and f2.kind_of?(Numeric)
+       f1 < f2 
+    end
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -23,13 +41,38 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+    rating_list.split(', ').each do |rating|
+        if uncheck
+           step("I uncheck \"ratings_#{rating}\"")  
+        else
+           step("I check \"ratings_#{rating}\"")
+       end
+    end
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
 end
 
 Then /I should see all the movies/ do
+    @movies_at_side = []
+    all('#movies tr > td:nth-child(1)').each do |movie|
+       @movies_at_side << movie.text.to_s
+    end
+    
+    Movie.all.each do |movie|
+       @movies_at_side.should include movie[:title].to_s 
+    end
   # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+end
+
+Then(/^I should see movies with rating (.*)$/) do |rating_list|
+  all('#movies tr > td:nth-child(2)').each do |td|
+    rating_list.should include td.text
+  end
+end
+
+Then(/^I should not see movies with rating (.*)$/) do |rating_list|
+  all('#movies tr > td:nth-child(2)').each do |td|
+    not rating_list.include? td.text
+  end
 end
